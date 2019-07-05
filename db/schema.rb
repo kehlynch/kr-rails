d5988732b10c16fd70acea29e38f9e8aae766a7d
kehlynch <kelynch@gmail.com>
2019-07-05 15:20:25 +0100

    windows stuff

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_616_202_704) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2019_06_16_202704) do
 
-  create_table 'games', force: :cascade do |t|
-    t.json 'data'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.json "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
