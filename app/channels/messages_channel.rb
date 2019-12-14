9fe5fa6de0b87da5e0739d0234459c11d0c87b96
Kat Lynch <kat.lynch@futurelearn.com>
2019-12-14 15:14:28 +0000

    fix for crash on scores?

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,7 +1,9 @@
 class MessagesChannel < ApplicationCable::Channel
 	def subscribed
-    game = Game.find(params[:id])
-    stream_for game
+    if (params[:id])
+      game = Game.find(params[:id])
+      stream_for game
+    end
 	end
 
 	def receive
