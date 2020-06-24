904f93e75f471ad7ad0ba6615096734e43453f15
Kat Lynch <kat.lynch@futurelearn.com>
2020-06-24 18:45:41 +0100

    remove metric logger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-  around_action :collect_metrics
-
-  def collect_metrics
-    # ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |*args|
-    #   event = ActiveSupport::Notifications::Event.new(*args)
-    #   Rails.logger.info "Event received: #{event}"
-    # end
-    start = Time.now
-    yield
-    duration = Time.now - start
-    Rails.logger.info "#{controller_name}##{action_name}: #{duration}s"
-  end
 end
