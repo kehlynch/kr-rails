7041ec6779821852d30157bb39dcf070e7fbba07
Kat Lynch <kehlynch@gmail.com>
2021-01-02 11:05:44 +0000

    sorted hand

--- app/javascript/components/play/Hand.tsx
+++ app/javascript/components/play/Hand.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+
+import { sortCards } from "../../utils";
 import Card from "./Card";
 import { CardType } from "../../types";
 import styles from "../../styles/play/Hand.module.scss";
@@ -11,7 +13,7 @@ type HandProps = {
 const Hand = ({ cards, playCard }: HandProps): React.ReactElement => {
   return (
     <div className={styles.container}>
-      { cards.map((c) => <Card hand slug={c.slug} key={c.slug} onclick={() => playCard && playCard(c.slug) } /> ) }
+      { sortCards(cards).map((c) => <Card hand slug={c.slug} key={c.slug} onclick={() => playCard && playCard(c.slug) } /> ) }
     </div>
   );
 };
--- app/javascript/utils.ts
+++ app/javascript/utils.ts
@@ -1,5 +1,6 @@
 import {
   AnnouncementSlug,
+  CardType,
   CompassName,
   WinningBidSlug,
   BidSlug,
@@ -56,4 +57,21 @@ export const compassName = (
     CompassName.North,
     CompassName.West,
   ][mod(position - myPosition, 4)];
+};
+
+export const sortCards = (cards: Array<CardType>): Array<CardType> => {
+  return cards.sort((c1, c2) => {
+    const suitOrder = ["t", "h", "s", "d", "c"];
+    const suitSort = (slug: string): number => suitOrder.indexOf(slug[0]);
+    const cardValue = (slug: string): number => {
+      const value = parseInt(slug.split("_")[1], 10);
+      return value;
+    };
+    const suitSort1 = suitSort(c1.slug);
+    const suitSort2 = suitSort(c2.slug);
+    if (suitSort1 === suitSort2) {
+      return cardValue(c1.slug) > cardValue(c2.slug) ? -1 : 1;
+    }
+    return suitSort1 > suitSort2 ? 1 : -1;
+  });
 };
